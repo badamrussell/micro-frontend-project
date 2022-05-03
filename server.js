4bb1e00775046fd52b3170e1ff8515cfef47fb5c
adam russell <badamrussell@users.noreply.github.com>
2022-05-03 14:58:26 -0400

    chore: removed comments and reorganized

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const app = express();
 const port = 3000;
 
 app.use('/mfe/music', express.static(path.join(__dirname, 'music', 'dist/')));
-// app.use('/mfe/music/assets', express.static(path.join(__dirname, 'music', 'dist/assets/')));
 app.use('/mfe/welcome', express.static(path.join(__dirname, 'welcome', 'dist/')));
-// app.use('/mfe/welcome/assets', express.static(path.join(__dirname, 'welcome', 'dist/assets/')));
 app.use('/', express.static(path.join(__dirname, 'bootstrap', 'dist/')));
 
 app.listen(port, () => {
